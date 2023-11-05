7525ebdfce745ae7d4c97ecb9bed81101bef75f2
skedwards88 <skedwards88@github.com>
2023-11-05 07:27:29 -0800

    add tests and some edge case handling for crosswordValidQ

--- src/crosswordValidQ.js
+++ src/crosswordValidQ.js
@@ -50,6 +50,12 @@ function isSingleGroupingQ(grid) {
       break;
     }
   }
+
+  // If the grid is empty, just return true
+  if (startingIndex === undefined) {
+    return true;
+  }
+
   let connectionsToCheckForConnections = getSurroundingLetterIndexes({
     startingIndex: startingIndex,
     grid: grid,
@@ -83,6 +89,20 @@ export function crosswordValidQ({ grid, trie, exceptedWords = [] }) {
       gameIsSolved: false,
       reason: `All of the letters must connect`,
     };
+  }
+
+  // The simple method of grid transposition used in this function
+  // relies on an equal number of rows and columns,
+  // so throw errors if that isn't the case
+  if (grid.length != grid[0]?.length) {
+    throw new Error(
+      `The number of columns and number of rows in the grid must be equal.`
+    );
+  }
+
+  const numColumnsPerRow = grid.map((row) => row.length);
+  if (Math.min(...numColumnsPerRow) != Math.max(...numColumnsPerRow)) {
+    throw new Error(`All of the rows in the grid must have the same length.`);
   }
 
   const transposedGrid = grid.map((_, index) => grid.map((row) => row[index]));
