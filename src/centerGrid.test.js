22fd99f66baa1b6af51f72a4f641405ef72e65db
skedwards88 <skedwards88@github.com>
2024-03-24 09:25:00 -0700

    specify empty value for centerGrid and getMaxShifts

--- src/centerGrid.test.js
+++ src/centerGrid.test.js
@@ -9,7 +9,7 @@ test("Centers a 2D grid of letters", () => {
     ["F", "B", "", "", ""],
   ];
 
-  const centeredGrid = centerGrid(grid);
+  const centeredGrid = centerGrid(grid, "");
 
   const expectedGrid = [
     ["", "", "", "", ""],
@@ -17,6 +17,51 @@ test("Centers a 2D grid of letters", () => {
     ["", "E", "", "C", ""],
     ["", "F", "B", "", ""],
     ["", "", "", "", ""],
+  ];
+
+  expect(centeredGrid).toEqual(expectedGrid);
+});
+
+test("Centers a 2D grid where the empty values are 0 instead of an empty string", () => {
+  const grid = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [1, 0, 1, 0, 0],
+    [1, 1, 0, 0, 0],
+  ];
+
+  const centeredGrid = centerGrid(grid, 0);
+
+  const expectedGrid = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 1, 0, 1, 0],
+    [0, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0],
+  ];
+
+  expect(centeredGrid).toEqual(expectedGrid);
+});
+
+test("Centers a 2D grid where the empty values are undefined instead of an empty string", () => {
+  const grid = [
+    [undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined],
+    [undefined, "DOG", undefined, undefined, undefined],
+    ["ELK", undefined, "COW", undefined, undefined],
+    ["FISH", "BIRD", undefined, undefined, undefined],
+  ];
+
+  const centeredGrid = centerGrid(grid, undefined);
+
+  console.log(centeredGrid);
+  const expectedGrid = [
+    [undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, "DOG", undefined, undefined],
+    [undefined, "ELK", undefined, "COW", undefined],
+    [undefined, "FISH", "BIRD", undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined],
   ];
 
   expect(centeredGrid).toEqual(expectedGrid);
@@ -41,7 +86,7 @@ test("If the grid can't be perfectly centered, errs towards the top left", () =>
     ["", "D", "", "", "", ""],
   ];
 
-  expect(centerGrid(gridBottomLeft)).toEqual(expectedGrid);
+  expect(centerGrid(gridBottomLeft, "")).toEqual(expectedGrid);
 
   const gridBottomRight = [
     ["", "", "", "", "", ""],
@@ -52,7 +97,7 @@ test("If the grid can't be perfectly centered, errs towards the top left", () =>
     ["", "", "", "", "D", ""],
   ];
 
-  expect(centerGrid(gridBottomRight)).toEqual(expectedGrid);
+  expect(centerGrid(gridBottomRight, "")).toEqual(expectedGrid);
 
   const gridTopLeft = [
     ["", "B", "", "", "", ""],
@@ -63,7 +108,7 @@ test("If the grid can't be perfectly centered, errs towards the top left", () =>
     ["", "", "", "", "", ""],
   ];
 
-  expect(centerGrid(gridTopLeft)).toEqual(expectedGrid);
+  expect(centerGrid(gridTopLeft, "")).toEqual(expectedGrid);
 
   const gridTopRight = [
     ["", "", "", "", "B", ""],
@@ -74,7 +119,7 @@ test("If the grid can't be perfectly centered, errs towards the top left", () =>
     ["", "", "", "", "", ""],
   ];
 
-  expect(centerGrid(gridTopRight)).toEqual(expectedGrid);
+  expect(centerGrid(gridTopRight, "")).toEqual(expectedGrid);
 });
 
 test("If the grid is already centered up/down, will still center right/left", () => {
@@ -86,7 +131,7 @@ test("If the grid is already centered up/down, will still center right/left", ()
     ["", "", "", "", ""],
   ];
 
-  const centeredGrid = centerGrid(grid);
+  const centeredGrid = centerGrid(grid, "");
 
   const expectedGrid = [
     ["", "", "", "", ""],
@@ -108,7 +153,7 @@ test("If the grid is already centered right/left, will still center up/down", ()
     ["", "F", "B", "", ""],
   ];
 
-  const centeredGrid = centerGrid(grid);
+  const centeredGrid = centerGrid(grid, "");
 
   const expectedGrid = [
     ["", "", "", "", ""],
@@ -131,7 +176,7 @@ test("Errors if the grid is not a square", () => {
     ["A", "", "C", "", ""],
   ];
 
-  expect(() => centerGrid(grid)).toThrow(
+  expect(() => centerGrid(grid, "")).toThrow(
     "The number of columns and number of rows in the grid must be equal.",
   );
 });
--- src/getMaxShifts.test.js
+++ src/getMaxShifts.test.js
@@ -10,8 +10,52 @@ test("Gets the number of empty rows/columns on each side of a 2D grid", () => {
     ["", "A", "", "C", "", ""],
   ];
 
-  const {maxShiftLeft, maxShiftRight, maxShiftUp, maxShiftDown} =
-    getMaxShifts(grid);
+  const {maxShiftLeft, maxShiftRight, maxShiftUp, maxShiftDown} = getMaxShifts(
+    grid,
+    "",
+  );
+
+  expect(maxShiftLeft).toEqual(1);
+  expect(maxShiftRight).toEqual(2);
+  expect(maxShiftUp).toEqual(1);
+  expect(maxShiftDown).toEqual(0);
+});
+
+test("Gets the number of empty rows/columns on each side of a 2D grid where the empty values are 0", () => {
+  const grid = [
+    [0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 0],
+    [0, 1, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0, 0],
+    [0, 1, 0, 1, 0, 0],
+  ];
+
+  const {maxShiftLeft, maxShiftRight, maxShiftUp, maxShiftDown} = getMaxShifts(
+    grid,
+    0,
+  );
+
+  expect(maxShiftLeft).toEqual(1);
+  expect(maxShiftRight).toEqual(2);
+  expect(maxShiftUp).toEqual(1);
+  expect(maxShiftDown).toEqual(0);
+});
+
+test("Gets the number of empty rows/columns on each side of a 2D grid where the empty values are undefined", () => {
+  const grid = [
+    [undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, "ANT", undefined, "CROW", undefined, undefined],
+    [undefined, "ART", "BALLOON", "CAR", undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, "AWL", "ZIP", "CAT", undefined, undefined],
+    [undefined, "ARF", undefined, "COw", undefined, undefined],
+  ];
+
+  const {maxShiftLeft, maxShiftRight, maxShiftUp, maxShiftDown} = getMaxShifts(
+    grid,
+    undefined,
+  );
 
   expect(maxShiftLeft).toEqual(1);
   expect(maxShiftRight).toEqual(2);
@@ -28,8 +72,10 @@ test("Returns 0 in all directions if grid can't be shifted", () => {
     ["A", "", "C", "", ""],
   ];
 
-  const {maxShiftLeft, maxShiftRight, maxShiftUp, maxShiftDown} =
-    getMaxShifts(grid);
+  const {maxShiftLeft, maxShiftRight, maxShiftUp, maxShiftDown} = getMaxShifts(
+    grid,
+    "",
+  );
 
   expect(maxShiftLeft).toEqual(0);
   expect(maxShiftRight).toEqual(0);
@@ -47,7 +93,7 @@ test("Errors if the grid is not a square", () => {
     ["A", "", "C", "", ""],
   ];
 
-  expect(() => getMaxShifts(grid)).toThrow(
+  expect(() => getMaxShifts(grid, "")).toThrow(
     "The number of columns and number of rows in the grid must be equal.",
   );
 });
