8b200c36814b38547076350e1e918b176d9966a2
Sarah Edwards <skedwards88@gmail.com>
2026-06-26 08:58:16 -0700

    try moving index.js into src

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+export {arraysMatchQ} from "./arraysMatchQ.js";
+export {getSurroundingIndexes} from "./getSurroundingIndexes.js";
+export {checkIfNeighbors} from "./checkIfNeighbors.js";
+export {getTrie} from "./getTrie.js";
+export {isKnown} from "./isKnown.js";
+export {crosswordValidQ} from "./crosswordValidQ.js";
+export {findAllWordIndexes, findAllWords} from "./findAllWords.js";
+export {getLetterPool} from "./getLetterPool.js";
+export {getPositionalFractions} from "./getPositionalFractions.js";
+export {partitionArray} from "./partitionArray.js";
+export {shuffleArray} from "./shuffleArray.js";
+export {sortLettersBy} from "./sortLetters.js";
+export {transposeGrid} from "./transposeGrid.js";
+export {getNLetters} from "./getNLetters.js";
+export {
+  pickRandomItemFromArray,
+  pickRandomIndexFromArray,
+  pickRandomItemsFromArray,
+  pickRandomIntBetween,
+} from "./pickRandom.js";
+export {getMaxShifts} from "./getMaxShifts.js";
+export {centerGrid} from "./centerGrid.js";
+export {arrayToGrid} from "./arrayToGrid.js";
